2c5f6cdae1b925425bfc4848a9aff917c85446e5
Rubén Caro <ruben.caro.estevez@gmail.com>
2014-11-09 08:59:04 +0100

    Cleanup mix.exs

--- mix.exs
+++ mix.exs
@@ -4,15 +4,7 @@ defmodule Cipher.Mixfile do
   def project do
     [app: :cipher,
      version: "0.0.1",
-     elixir: "~> 1.0",
-     deps: deps]
+     elixir: "~> 1.0"]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
-  def application, do: []
-
-  # Type `mix help deps` for more examples and options
-  defp deps, do: []
 end
