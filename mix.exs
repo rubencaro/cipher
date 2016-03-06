879272088b2e5606139f09f8f68465770c1d4b6b
Rubén Caro <ruben.caro.estevez@gmail.com>
2016-03-06 09:16:10 +0100

    WIP road to 1.0

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Cipher.Mixfile do
 
   def project do
     [app: :cipher,
-     version: "0.1.0",
-     elixir: "~> 1.0",
+     version: "1.0.0",
+     elixir: "~> 1.2",
      package: package,
      description: """
         Elixir crypto library to encrypt/decrypt arbitrary binaries. It uses
@@ -12,7 +12,8 @@ defmodule Cipher.Mixfile do
         wrapping it. It allows to use a crypted key to validate signed requests.
         The exact same cipher is implemented for Python, Ruby and Elixir, so it
         can be used to integrate apps from different languages.
-      """]
+      """,
+      deps: deps]
   end
 
   defp package do
@@ -20,6 +21,10 @@ defmodule Cipher.Mixfile do
      licenses: ["MIT"],
      links: %{github: "https://github.com/rubencaro/cipher",
               other_languages: "https://gist.github.com/rubencaro/9545060"}]
+  end
+
+  defp deps do
+    [{:poison, "~> 2.0", only: :test}]
   end
 
 end
--- test/cipher_test.exs
+++ test/cipher_test.exs
@@ -3,24 +3,57 @@ defmodule CipherTest do
   alias Cipher, as: C
 
   test "the whole encrypt/decrypt stack" do
-    s = "hola qué tal ｸｿ"
+    s = Poison.encode! %{"hola": " qué tal ｸｿ"}
     assert s == s |> C.encrypt(k,i) |> C.decrypt(k,i)
   end
 
-  test "url signature" do
+  test "parse ciphered hash" do
+    h = %{"hola": " qué tal ｸｿ"}
+    s = Poison.encode! h
+
+    res = s |> C.encrypt(k,i) |> C.parse(k,i)
+    assert res.valid
+    assert res.data == h
+
+    res = C.parse 'very invalid', k, i)
+    refute res.valid
+
+    res = C.parse(C.encrypt(s,k,i)) <> "slightly invalid", k, i)
+    refute res.valid
+  end
+
+  test "get ciphered hash" do
+    h = %{"hola": " qué tal ｸｿ"}
+    s = h |> Poison.encode! |> C.encrypt
+    assert C.cipher(h) == s
+  end
+
+  test "validate_signed_url" do
     # ok with regular urls
-    url = "/blab/bla"
-    assert url |> C.sign_url(k,i) |> C.validate_signed_url(k,i)
-    url = "/blab/bla?sdfgsdf=dfgsd"
-    assert url |> C.sign_url(k,i) |> C.validate_signed_url(k,i)
+    assert "/bla/bla" |> C.sign_url(k,i) |> C.validate_signed_url(k,i)
+    assert "/bla/bla?sdfasdf=sdfgadf&dsfasdf=addfga" |> C.sign_url(k,i) |> C.validate_signed_url(k,i)
 
-    # not signed fails
-    url = "/blab/bla"
-    refute C.validate_signed_url(url,k,i)
+    # not signed and wrongly signed fails
+    refute "/bla/bla" |> C.validate_signed_url(k,i)
+    refute "/bla/bla?signature=badhash" |> C.validate_signed_url(k,i)
+    refute "/bla/bla?asdkjh=sdfklh&signature=badhash" |> C.validate_signed_url(k,i)
+  end
 
-    # bad signature fails
-    signed = url <> "?signature=badhash"
-    refute C.validate_signed_url(signed,k,i)
+  test "it works ignoring some too" do
+    s = "/bla/bla" |> C.sign_url(k, i, ignored: ["source"])
+    assert C.validate_signed_url(s, k, i)
+    assert C.validate_signed_url(s <> "&source=crappysource", k, i)
+    refute C.validate_signed_url(s <> "&other=crappysource", k, i)
+  end
+
+  test "Magic Token works" do
+    assert "/bla/bla?a=123&signature=#{C.magic_token}" |> C.validate_signed_url(k,i)
+    refute "/bla/bla?a=123&signature=#{C.magic_token}X" |> C.validate_signed_url(k,i)
+  end
+
+  test "validate_signed_body" do
+    body = Poison.encode! %{"hola": " qué tal ｸｿ"}
+    
   end
 
   # handy to have them around
