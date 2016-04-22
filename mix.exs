6dfe85a228030014bfcd16b7a9250ec17284cde9
Rubén Caro <ruben.caro.estevez@gmail.com>
2016-04-22 19:44:32 +0100

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Cipher.Mixfile do
 
   def project do
     [app: :cipher,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.2",
      package: package,
      description: """
