eda34056b7dd1f8c1324d326ab848bd93d3917e9
Rubén Caro <ruben.caro.estevez@gmail.com>
2017-08-04 14:58:34 +0200

    Add github links to generated docs

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Cipher.Mixfile do
      elixir: ">= 1.3.0",
      package: package(),
      description: "Elixir crypto library to encrypt/decrypt arbitrary binaries.",
+     source_url: "https://github.com/rubencaro/cipher",
+     docs: [source_ref: get_commit()],
      deps: deps()]
   end
 
@@ -19,8 +21,12 @@ defmodule Cipher.Mixfile do
 
   defp deps do
     [{:poison, "~> 2.0 or ~> 3.0"},
-     {:ex_doc, ">= 0.0.0", only: :dev},
+     {:ex_doc, "~> 0.14", only: :dev, runtime: false},
      {:plug, ">= 1.1.0"}]
+  end
+
+  defp get_commit do
+    :os.cmd('git rev-parse --short HEAD') |> to_string |> String.trim_trailing("\n")
   end
 
 end
